d83a4b96ee67f0dfe5e7ba394b2159ef7730ed36
Michal Migurski <mike@teczno.com>
2026-04-03 19:36:46 -0700

    Removed cache buster

--- cpp/build.sh
+++ cpp/build.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"
 
-docker build --platform linux/arm64 --target builder --build-arg CACHE_BUST="$(date +%s)" -t atgeo-cpp-builder .
+docker build --platform linux/arm64 --target builder -t atgeo-cpp-builder .
 
 CONTAINER_ID=$(docker create atgeo-cpp-builder sh)
 docker cp "${CONTAINER_ID}:/lambda-cpp.zip" ../lambda-cpp.zip
